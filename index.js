8e2d65f0953cee36956f1991eb6843d1d38141f5
Sarder <rahmantubab2@gmail.com>
2024-12-21 19:40:19 -0800

    save

--- index.js
+++ index.js
@@ -4,10 +4,9 @@ window.addEventListener('load', function() {
 
 // THREE.JS
 import './style.css';
-import * as THREE from 'three';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
-import { materialOpacity } from 'three/examples/jsm/nodes/Nodes.js';
+import * as THREE from './node_modules/three/build/three.module.js';
+import { OrbitControls } from './node_modules/three/examples/jsm/controls/OrbitControls.js';
+import { GLTFLoader } from './node_modules/three/examples/jsm/loaders/GLTFLoader.js';
 
 const scene = new THREE.Scene();
 
